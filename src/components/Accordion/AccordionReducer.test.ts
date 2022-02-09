993c23581b83d097a5427051107748a075dbc85e
egory4fash <egormaksul@gmail.com>
2022-02-09 11:00:06 +0300

    kabzda 15 done+

--- src/components/Accordion/AccordionReducer.test.ts
+++ src/components/Accordion/AccordionReducer.test.ts
@@ -10,3 +10,18 @@ const newState = AccordionReducer(state,{type:TOGGLE_CONSTANT})
     expect(newState.collapsed).toBe(!state.collapsed)
 })
 
+
+test('error raised by incorrect action', () => {
+
+    const state:StateType = {
+        collapsed:false
+    }
+
+    expect( () => {
+        AccordionReducer(state,{type:"TOGGLE_CONSTANTE"})
+    }).toThrowError()
+
+})
+
+
+
